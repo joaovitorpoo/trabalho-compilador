e0efefb07d0c3beb02fb47a638b646f9a43adfdd
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-05-22 09:26:05 -0300

    main / tag / token / ts

---
+++ trabalho-compilador/main.py
@@ -0,0 +1,22 @@
+from tag import Tag
+from token import Token
+from analisador import Analisador
+
+if __name__ == "__main__":
+
+    analisador = Analisador('fonte.txt')
+
+    print("\n=>Lista de tokens:")
+    token = analisador.proxToken()
+
+    while (token is not None and token.getNome() != Tag.EOF):
+        print(token.toString(), "Linha: " + str(token.getLinha()) + " Coluna: " + str(token.getColuna()))
+        token = analisador.proxToken()
+    print(token.toString(), "Linha: " + str(token.getLinha()) + " Coluna: " + str(token.getColuna()))
+
+
+    print("\n=>Tabela de simbolos:")
+    analisador.printTS()
+    analisador.closeFile()
+
+    print('\n=> Fim da compilacao')
